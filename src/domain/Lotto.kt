32e135a5cde281c879f161c1c7c204c5804d951d
cocojisoo <yjs1019a@gmail.com>
2026-04-25 03:57:04 +0900

    feat(domain) : Lotto 도메인 예외처리 추가

--- src/domain/Lotto.kt
+++ src/domain/Lotto.kt
@@ -1,5 +1,18 @@
 package domain
 
 class Lotto(private val numbers : List<Int>){
-    
+    init{
+        validateSize(numbers)
+        validateRange(numbers)
+        validateDuplicate(numbers)
+    }
+    private fun validateSize(numbers: List<Int>){
+        if(numbers.size != 6) throws IllegalArgumentException("[ERROR] 번호는 6개여야합니다!")
+    }
+    private fun validateRange(numbers: List<Int>){
+        if(numbers.all{it !in 1..45}) throws IllegalArgumentException("[ERROR] 번호는 1 ~ 45 사이의 수여야 합니다!")
+    }
+    private fun validateDuplicate(numbers: List<Int>){
+        if(numbers.distinct().size != 6) throws IllegalArgumentException("[ERROR] 번호는 중복되어선 안됩니다!")
+    }
 }

346a24d2811477bb2987a41759835c93f088ed07
cocojisoo <yjs1019a@gmail.com>
2026-04-25 18:13:44 +0900

    refactor : throw  문법 오류 수정

--- src/domain/Lotto.kt
+++ src/domain/Lotto.kt
@@ -7,12 +7,12 @@ class Lotto(private val numbers : List<Int>){
         validateDuplicate(numbers)
     }
     private fun validateSize(numbers: List<Int>){
-        if(numbers.size != 6) throws IllegalArgumentException("[ERROR] 번호는 6개여야합니다!")
+        if(numbers.size != 6) throw IllegalArgumentException("[ERROR] 번호는 6개여야합니다!")
     }
     private fun validateRange(numbers: List<Int>){
-        if(numbers.all{it !in 1..45}) throws IllegalArgumentException("[ERROR] 번호는 1 ~ 45 사이의 수여야 합니다!")
+        if(numbers.all{it !in 1..45}) throw IllegalArgumentException("[ERROR] 번호는 1 ~ 45 사이의 수여야 합니다!")
     }
     private fun validateDuplicate(numbers: List<Int>){
-        if(numbers.distinct().size != 6) throws IllegalArgumentException("[ERROR] 번호는 중복되어선 안됩니다!")
+        if(numbers.distinct().size != 6) throw IllegalArgumentException("[ERROR] 번호는 중복되어선 안됩니다!")
     }
 }
